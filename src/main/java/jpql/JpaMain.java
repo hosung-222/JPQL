17713d7e4bf2d06c07a0c9972b480c5494e82239
hosung-222 <gik11kr@naver.com>
2023-06-21 00:49:43 +0900

    add paging

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -7,14 +7,17 @@ public class JpaMain {
     public static void main(String [] args){
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager em = emf.createEntityManager();
-
         EntityTransaction tx = em.getTransaction();
         tx.begin();
+
         try{
-            Member member = new Member();
-            member.setUsername("member1");
-            member.setAge(10);
-            em.persist(member);
+            for (int i = 0; i <100 ; i++) {
+                Member member = new Member();
+                member.setUsername("member" + i);
+                member.setAge(i);
+                em.persist(member);
+            }
+
 
             //반환 타입
 //            //TypedQuery : 반환 타입이 명확할 때 사용
@@ -80,6 +83,20 @@ public class JpaMain {
 //            MemberDTO memberDTO = result2.get(0);
 //            System.out.println("username = " + memberDTO.getUsername());
 //            System.out.println("age = " + memberDTO.getAge());
+
+
+            //페이징
+//            //** setFirstResult : 조회 시작 위치, setMaxResult : 조회할 데이터 수 **
+//            List<Member> result = em.createQuery("select m from Member m order by m.age desc ",Member.class)
+//                            .setFirstResult(1)
+//                                    .setMaxResults(10)
+//                                            .getResultList();
+//            System.out.println("result.size = " + result.size());
+//            for (Member member1: result) {
+//                System.out.println("member1 = " + member1);
+//            }
+
+
 
 
             em.flush();
--- src/main/java/jpql/MemberDTO.java
+++ src/main/java/jpql/MemberDTO.java
@@ -24,4 +24,5 @@ public class MemberDTO {
     public void setAge(int age) {
         this.age = age;
     }
+
 }
